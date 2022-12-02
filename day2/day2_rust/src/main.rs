613a18a968c225549b726de8bf021bc772a833ed
frectonz <fraol0912@gmail.com>
2022-12-02 10:27:21 +0300

    add types

--- day2/day2_rust/src/main.rs
+++ day2/day2_rust/src/main.rs
@@ -1,24 +1,37 @@
+#[derive(Debug)]
+enum GameMove {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+#[derive(Debug)]
+enum GameEnd {
+    Win,
+    Lose,
+    Draw,
+}
+
 fn main() {
     let total_score = std::fs::read_to_string("../data")
         .unwrap()
         .lines()
         .fold(0, |acc, line| {
-            let mut line = line.split(" ");
+            let mut line = line.split(' ');
             let opponent_move = line.next().unwrap();
-            let opponent_move = parse_my_move(opponent_move);
+            let opponent_move = parse_abc(opponent_move);
 
             let game_end = line.next().unwrap();
-            let game_end = parse_game_end(game_end);
+            let game_end = parse_xyz_to_game_end(game_end);
 
-            let my_move = calculate_my_move(opponent_move, game_end);
+            let my_move = calculate_my_move(&opponent_move, &game_end);
 
-            let my_score = calculate_win_score(my_move, opponent_move);
+            let my_score = calculate_win_score(&my_move, &opponent_move);
 
             let score = match my_move {
-                "rock" => my_score + 1,
-                "paper" => my_score + 2,
-                "scissors" => my_score + 3,
-                _ => my_score,
+                GameMove::Rock => my_score + 1,
+                GameMove::Paper => my_score + 2,
+                GameMove::Scissors => my_score + 3,
             };
 
             acc + score
@@ -27,68 +40,73 @@ fn main() {
     dbg!(total_score);
 }
 
-fn parse_my_move(s: &str) -> &str {
+fn parse_abc(s: &str) -> GameMove {
+    use GameMove::*;
     if s == "A" {
-        "rock"
+        Rock
     } else if s == "B" {
-        "paper"
+        Paper
     } else if s == "C" {
-        "scissors"
+        Scissors
     } else {
-        panic!("Invalid move: {}", s);
+        panic!("Invalid move")
     }
 }
 
-fn parse_opponent_move(s: &str) -> &str {
+// used for part 1
+fn _parse_xyz(s: &str) -> GameMove {
+    use GameMove::*;
     if s == "X" {
-        "rock"
+        Rock
     } else if s == "Y" {
-        "paper"
+        Paper
     } else if s == "Z" {
-        "scissors"
+        Scissors
     } else {
-        panic!("Invalid move: {}", s);
+        panic!("Invalid move")
     }
 }
 
-fn parse_game_end(s: &str) -> &str {
+fn parse_xyz_to_game_end(s: &str) -> GameEnd {
+    use GameEnd::*;
     if s == "X" {
-        "LOSE"
+        Lose
     } else if s == "Y" {
-        "DRAW"
+        Draw
     } else if s == "Z" {
-        "WIN"
+        Win
     } else {
         panic!("Invalid game end: {}", s);
     }
 }
 
-fn calculate_win_score(my_move: &str, opponent_move: &str) -> i32 {
+fn calculate_win_score(my_move: &GameMove, opponent_move: &GameMove) -> i32 {
+    use GameMove::*;
     match (my_move, opponent_move) {
-        ("rock", "scissors") => 6,
-        ("rock", "paper") => 0,
-        ("rock", "rock") => 3,
-        ("paper", "rock") => 6,
-        ("paper", "scissors") => 0,
-        ("paper", "paper") => 3,
-        ("scissors", "paper") => 6,
-        ("scissors", "rock") => 0,
-        ("scissors", "scissors") => 3,
-        _ => panic!("Invalid move combination: {} {}", my_move, opponent_move),
+        (Rock, Scissors) => 6,
+        (Rock, Paper) => 0,
+        (Rock, Rock) => 3,
+        (Paper, Rock) => 6,
+        (Paper, Scissors) => 0,
+        (Paper, Paper) => 3,
+        (Scissors, Paper) => 6,
+        (Scissors, Rock) => 0,
+        (Scissors, Scissors) => 3,
     }
 }
 
-fn calculate_my_move<'a>(opponent_move: &'a str, game_end: &'a str) -> &'a str {
+fn calculate_my_move(opponent_move: &GameMove, game_end: &GameEnd) -> GameMove {
+    use GameEnd::*;
+    use GameMove::*;
     match (opponent_move, game_end) {
-        ("rock", "WIN") => "paper",
-        ("rock", "DRAW") => "rock",
-        ("rock", "LOSE") => "scissors",
-        ("paper", "WIN") => "scissors",
-        ("paper", "DRAW") => "paper",
-        ("paper", "LOSE") => "rock",
-        ("scissors", "WIN") => "rock",
-        ("scissors", "DRAW") => "scissors",
-        ("scissors", "LOSE") => "paper",
-        _ => panic!("Invalid move combination: {} {}", opponent_move, game_end),
+        (Rock, Win) => Paper,
+        (Rock, Draw) => Rock,
+        (Rock, Lose) => Scissors,
+        (Paper, Win) => Scissors,
+        (Paper, Draw) => Paper,
+        (Paper, Lose) => Rock,
+        (Scissors, Win) => Rock,
+        (Scissors, Draw) => Scissors,
+        (Scissors, Lose) => Paper,
     }
 }
