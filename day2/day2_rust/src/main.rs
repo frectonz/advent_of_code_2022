04d89604d5475dd118a8d41718a09e548f3cae84
frectonz <fraol0912@gmail.com>
2022-12-02 09:56:42 +0300

    day part 2 completed with rust

--- day2/day2_rust/src/main.rs
+++ day2/day2_rust/src/main.rs
@@ -7,8 +7,10 @@ fn main() {
             let opponent_move = line.next().unwrap();
             let opponent_move = parse_my_move(opponent_move);
 
-            let my_move = line.next().unwrap();
-            let my_move = parse_opponent_move(my_move);
+            let game_end = line.next().unwrap();
+            let game_end = parse_game_end(game_end);
+
+            let my_move = calculate_my_move(opponent_move, game_end);
 
             let my_score = calculate_win_score(my_move, opponent_move);
 
@@ -49,6 +51,18 @@ fn parse_opponent_move(s: &str) -> &str {
     }
 }
 
+fn parse_game_end(s: &str) -> &str {
+    if s == "X" {
+        "LOSE"
+    } else if s == "Y" {
+        "DRAW"
+    } else if s == "Z" {
+        "WIN"
+    } else {
+        panic!("Invalid game end: {}", s);
+    }
+}
+
 fn calculate_win_score(my_move: &str, opponent_move: &str) -> i32 {
     match (my_move, opponent_move) {
         ("rock", "scissors") => 6,
@@ -61,5 +75,20 @@ fn calculate_win_score(my_move: &str, opponent_move: &str) -> i32 {
         ("scissors", "rock") => 0,
         ("scissors", "scissors") => 3,
         _ => panic!("Invalid move combination: {} {}", my_move, opponent_move),
+    }
+}
+
+fn calculate_my_move<'a>(opponent_move: &'a str, game_end: &'a str) -> &'a str {
+    match (opponent_move, game_end) {
+        ("rock", "WIN") => "paper",
+        ("rock", "DRAW") => "rock",
+        ("rock", "LOSE") => "scissors",
+        ("paper", "WIN") => "scissors",
+        ("paper", "DRAW") => "paper",
+        ("paper", "LOSE") => "rock",
+        ("scissors", "WIN") => "rock",
+        ("scissors", "DRAW") => "scissors",
+        ("scissors", "LOSE") => "paper",
+        _ => panic!("Invalid move combination: {} {}", opponent_move, game_end),
     }
 }
